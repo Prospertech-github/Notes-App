dbc09f112d73b08f230fc53017c9be94c2d9d047
Prosper Ikechukwu <mrhendrix62@gmail.com>
2023-02-25 23:10:42 +0100

    Feat: Completed Add Note and Delete Note functionalities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import { useState } from 'react';
+import uuid from 'react-uuid';
 import './App.css';
 import Main from './components/Main';
 import NotesContainer from './components/NotesContainer';
 
 function App() {
-	const [notes, setNotes] = useState([1, 2, 4, 3]);
+	const [notes, setNotes] = useState([]);
+
+	const addNote = () => {
+		const newNote = {
+			id: uuid(),
+			title: 'Note Title',
+			body: 'lorem ipsum untitled',
+			modifiedDate: Date.now(),
+		};
+
+		setNotes([newNote, ...notes]);
+	};
+
+  const deleteNote = (noteId) => {
+    setNotes(notes.filter(({ id }) => id !== noteId));
+  };
+
 	return (
 		<div className="App">
-			<NotesContainer notes={notes} />
+			<NotesContainer
+				notes={notes}
+				onAddNote={addNote}
+				onDeleteNote={deleteNote}
+			/>
 			<Main />
 		</div>
 	);
--- src/components/NotesContainer.jsx
+++ src/components/NotesContainer.jsx
@@ -1,35 +1,41 @@
 import React from 'react';
 import styles from './NotesContainer.module.css';
 
-const NotesContainer = ({ notes }) => {
+const NotesContainer = ({ notes, onAddNote, onDeleteNote }) => {
 	return (
 		<div className={styles.notesContainer}>
 			<div className={styles.header}>
 				<h2> My Notes App </h2>
 
-				<button> Add </button>
+				<button onClick={onAddNote}> Add </button>
 			</div>
 
 			<div className={styles.notesHolder}>
 				{notes.map(note => {
 					return (
-						<div className={styles.note}>
+						<div className={styles.note} key={note.id}>
 							<div className={styles.noteHeader}>
-								<h3> Title </h3>
-								<button className={styles.btnDelete}>
+								<h3> {note.title} </h3>
+								<button
+									className={styles.btnDelete}
+									onClick={(e) => onDeleteNote(note.id)}
+								>
 									{' '}
 									Delete{' '}
 								</button>
 							</div>
 
-							<p className={styles.noteDetails}>
-								{' '}
-								Lorem ipsum dolor sit amet consectetur.
-							</p>
+							<p className={styles.noteDetails}>{note.body}</p>
 
 							<small className={styles.noteMeta}>
-								{' '}
-								Last Modified on {Date()}{' '}
+								Last Modified{' '}
+								{new Date(note.modifiedDate).toLocaleDateString(
+									'en-GB',
+									{
+										hour: '2-digit',
+										minute: '2-digit',
+									}
+								)}
 							</small>
 						</div>
 					);
